2a0e9bdf850b5e1789c9c25655de69ca6fbc58ef
vincent tene <vincent.tene@gmail.com>
2020-03-02 17:36:55 +0100

    refactoring

--- 03_2020_Cpp_Certified_Programmer_Open_University/01_Chapter01/01_Chapter01/01_Chapter01.cpp
+++ 03_2020_Cpp_Certified_Programmer_Open_University/01_Chapter01/01_Chapter01/01_Chapter01.cpp
@@ -1,11 +1,42 @@
 // 01_Chapter01.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
+#include <vector>
 #include <iostream>
+using namespace std;
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+
+	std::vector <int> v1(10);
+	vector <int> v2 = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+	for (unsigned i = 0; i < v1.size(); ++i)
+	{
+		v1[i] = i;
+	}
+	for (unsigned i = 0; i < v1.size(); ++i)
+	{
+		cout << v1[i] << " ";
+		cout << v2[i] << " ";
+	}
+	cout << endl;
+
+	for (auto elem : v1)
+	{
+		cout << "elem: " << elem << " " << endl;
+		cout << "elem: " << elem << " " << endl;
+	}
+
+	cout << endl;
+
+
+	cout << v1.size() << endl;
+	v1.push_back(100);
+	cout << v1.size() << endl;
+	v1.pop_back();
+	cout << v1.size() << endl;
+	return 0;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
